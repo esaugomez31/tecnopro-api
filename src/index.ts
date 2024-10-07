52b87253549a643224eabba61dcd328cb7a8b273
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2024-10-07 15:51:24 -0600

    fix: port variable

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import routes from './routes'
 import envs from './config/environment'
 
 const app = express()
-app.use(express.json())
+const port = envs.app.port
 
+app.use(express.json())
 app.use(routes)
 
 // Starting res API....
-app.listen(envs.app.port, () => {
-  return console.log(`Tecnopro API is listening at ${envs.app.port} port...`)
+app.listen(port, () => {
+  return console.log(`Tecnopro API is listening at ${port} port...`)
 })
