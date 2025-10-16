0d509c2a1b314cb4846fd21c55f13e295935e7e0
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2025-10-15 21:10:17 -0600

    fix(errors): no implicit any rule

--- src/errors/error.factory.ts
+++ src/errors/error.factory.ts
@@ -1,10 +1,11 @@
-const ErrorFactory = (name: string, msg: string): new (...args: any[]) => Error => {
+import { ErrorConstructor } from "../interfaces/error.interfaces"
+
+const ErrorFactory = (name: string, message: string): ErrorConstructor => {
   return class BusinessError extends Error {
-    constructor(message: string) {
+    constructor() {
       super(message)
+      Object.setPrototypeOf(this, new.target.prototype)
       this.name = name
-      this.stack = ""
-      this.message = msg
     }
   }
 }
