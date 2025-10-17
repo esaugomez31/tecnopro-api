79dc15ae8a11a5654d18fd03c8c4b0e09643cbf8
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2025-10-16 21:48:25 -0600

    fix(models): fix municipalities model

--- src/models/locations/municipalities.model.ts
+++ src/models/locations/municipalities.model.ts
@@ -9,29 +9,40 @@ import {
   JoinColumn,
 } from "typeorm"
 
-import { DepartmentModel } from "./departments.model"
+import { DepartmentModel, CountryModel } from "../index"
 
 @Entity("municipalities")
 export class MunicipalityModel extends BaseEntity {
-  @PrimaryGeneratedColumn({ name: "id_municipality" })
+  @PrimaryGeneratedColumn({ type: "int", name: "id_municipality" })
   idMunicipality?: number
 
-  @Column({ length: 85 })
-  name: string
+  @Column({ type: "varchar", length: 85 })
+  name!: string
 
-  @Column({ length: 10, nullable: true, name: "zip_code" })
-  zipCode?: string
+  @Column({ type: "varchar", length: 10, nullable: true, name: "zip_code" })
+  zipCode: string | null = null
 
-  @Column({ length: 2, nullable: true, name: "dte_code" })
-  dteCode?: string
+  @Column({ type: "varchar", length: 2, nullable: true, name: "dte_code" })
+  dteCode: string | null = null
 
-  @Column({ name: "id_country", nullable: true })
-  idCountry?: number
+  @Column({ type: "int", name: "id_country", nullable: true })
+  idCountry: number | null = null
 
-  @Column({ name: "id_department" })
-  idDepartment: number
+  @Column({ type: "int", name: "id_department" })
+  idDepartment!: number
 
-  @ManyToOne(() => DepartmentModel, (department) => department.municipalities)
+  @ManyToOne(() => CountryModel, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+    nullable: true,
+  })
+  @JoinColumn({ name: "id_country" })
+  country?: CountryModel
+
+  @ManyToOne(() => DepartmentModel, (department) => department.municipalities, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
   @JoinColumn({ name: "id_department" })
   department?: DepartmentModel
 
@@ -40,15 +51,16 @@ export class MunicipalityModel extends BaseEntity {
     type: "datetime",
     default: () => "CURRENT_TIMESTAMP",
   })
-  createdAt: Date
+  createdAt?: Date
 
   @UpdateDateColumn({
     name: "updated_at",
     type: "datetime",
     default: () => "CURRENT_TIMESTAMP",
+    onUpdate: "CURRENT_TIMESTAMP",
   })
-  updatedAt: Date
+  updatedAt?: Date
 
   @Column({ type: "tinyint", default: 1 })
-  status?: boolean
+  status: boolean = true
 }
